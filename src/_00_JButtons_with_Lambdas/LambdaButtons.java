cafa9c60dc747146cb6f13463ce92315bae79fdf
league <league@wts14-3.attlocal.net>
2024-07-13 18:01:15 -0700

    ja

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,9 +1,12 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class LambdaButtons {
 	private JFrame window = new JFrame();
@@ -20,6 +23,43 @@ public class LambdaButtons {
 		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
+		addNumbers.addActionListener((ActionEvent e)-> {
+			String input1 = JOptionPane.showInputDialog("Type first number");
+			String input2 = JOptionPane.showInputDialog("Type second number");
+			int num1 = Integer.parseInt(input1);
+			int num2 = Integer.parseInt(input2);
+			
+			System.out.println(num1 + num2);
+			
+			
+			
+		});
+		randNumber.addActionListener((ActionEvent e)-> {
+		Random 	ran = new Random();
+		int random = ran.nextInt(100);
+		System.out.println(random);
+		
+		
+		});
+		
+		tellAJoke.addActionListener((ActionEvent e)-> {
+			
+			
+			
+				String userInput = JOptionPane.showInputDialog("Why can't they keep score in Afghanistan?");
+				if(userInput.equalsIgnoreCase("Because of the tally ban") || userInput.equalsIgnoreCase("Because of the tally-ban")) {
+					JOptionPane.showMessageDialog(null, "Correct!");
+					
+				}else {
+					JOptionPane.showMessageDialog(null, "No, because of the tally ban!");
+				}
+			
+			
+			
+			
+			
+		});
+		
 		
 		window.setVisible(true);
 		window.pack();
--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -1,5 +1,9 @@
 package _01_Lambda_Methods;
 
+import java.util.ArrayList;
+
+import org.junit.jupiter.params.shadow.com.univocity.parsers.common.input.ElasticCharAppender;
+
 public class LambdaMethods {
 	public static void main(String[] args) {
 		// 1. Look at the SpecialPrinter function interface.
@@ -13,13 +17,80 @@ public class LambdaMethods {
 		}, "repeat");
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		
+		printCustomMessage((s)-> {
+			StringBuilder bob = new StringBuilder();
+			bob.append(s);
+			System.out.println(bob.reverse().toString());
+			
+			
+		}, "noah");
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
 		
+		
+		
+		printCustomMessage((noah)-> {
+			
+			StringBuilder bob = new StringBuilder(noah);
+			for(int i = 0; i < bob.toString().length(); i+=2) {
+
+				bob.setCharAt(i, Character.toUpperCase(bob.charAt(i)));
+				
+				
+			}
+			System.out.println(bob.toString());
+			
+			
+		}, "bingus");
+		
+		
+		
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
+		printCustomMessage((noah)-> {
+			
+			
+			char[] letters = noah.toCharArray() ;
+			
+			String finalThing = "";
+			
+			for(int i = 0; i < letters.length; i++) {
+				finalThing = finalThing + letters[i];
+				finalThing = finalThing + ".";
+				
+				
+			}
+			
+			StringBuilder bob = new StringBuilder(finalThing);
+			
+			//bob.delete(finalThing.length()-1, finalThing.length());
+			//looks better with the period at the end
+			
+			System.out.println(bob.toString());
+			
+		}, "Once Upon a Noah");
+		
+		
 		
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+	printCustomMessage((noh)-> {
+		
+		StringBuilder bob = new StringBuilder(noh);
+		
+		
+		for(int i = bob.length()-1; i >= 0 ; i--) {
+			
+			if(bob.charAt(i) == 'a' || bob.charAt(i) == 'e' ||bob.charAt(i) == 'i' ||bob.charAt(i) == 'o' ||bob.charAt(i) == 'u') {
+				bob.deleteCharAt(i);
+				
+				
+				
+			}
+		}
+		System.out.println(bob.toString());
+		
+		
+		
+		
+	}, "armani");
 	}
 	
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
